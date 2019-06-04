2c46d2968a1100812abbe88530e6d1682c0fed62
Ramon Paris <rparis@retailsbs.com>
2019-06-04 13:29:18 -0400

    primer middleware

---
+++ src/middleware/ForbiddenWordMiddleware.js
@@ -0,0 +1,18 @@
+import {ADD_ARTICLE} from "../constants/action-types";
+
+const forbiddenWords  = ["spam", "money"];
+
+export default function ForbiddenWordsMiddleware({ dispatch }) {
+    return function(next){
+        return function(action){
+            if (action.type === ADD_ARTICLE){
+                const foundWord = forbiddenWords.filter(word => action.payload.title.includes(word))
+
+                if (foundWord.length){
+                    return dispatch({type: 'FOUND_BAD_WORD'});
+                }
+            }
+            return next(action);
+        }
+    }
+}
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from '../reducers/ArticleReducer';
+import ForbiddenWordsMiddleware from "../middleware/ForbiddenWordMiddleware";
 
-const store = createStore(rootReducer);
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+const store = createStore(rootReducer, storeEnhancers(applyMiddleware(ForbiddenWordsMiddleware)));
 
 export default store;
